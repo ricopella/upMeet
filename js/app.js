52c406552b19600db6c85d6e799d2d8f669b9cee
Narin Sundarabhaya <nrs710@gmail.com>
2017-06-12 21:37:12 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function initMap() {
+
+  var theLat = 34.01839828491211;
+  var theLong = -118.48661041259766;
+
+  var uluru = { lat: theLat,
+                lng: theLong };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: uluru
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
