e69052cb933ceb66dd7a17c41c5388b25bdefca4
t1me1srunn1nout <hello@hovsep.io>
2017-06-16 15:24:09 -0700

    add google traffic layer and map markers for meetup results

--- js/apis.js
+++ js/apis.js
@@ -1,6 +1,6 @@
 console.log("in");
-// LA // searchMeetup("python", 40.728862, -73.996413);
-// NY // searchMeetup("javascript", 34.020204, -118.490765)
+// NY // searchAPIS("python", 40.728862, -73.996413);
+// LA // searchAPIS("javascript", 34.020204, -118.490765)
 
 var meetupResponse;
 var youtubeResponse;
@@ -13,12 +13,12 @@ function searchAPIS (search, lat, lon) {
   // call meetup
   $.ajax({
     url: "https://api.meetup.com/find/events?&sign=true&photo-host=public&lon=" +
-      lon + "&text="+ query + "&radius=50&lat=" + lat + "&key=13493128171b80333fc956a274b1c",
+      lon + "&text="+ query + "&radius=" + "30" + "&lat=" + lat + "&key=13493128171b80333fc956a274b1c",
     method: "GET",
   }).done(function(response){
-    theResponse = response;
     meetupResponse = response;
-    console.log(theResponse);
+    console.log(response);
+    console.log(response.length);
   });
 
   // call youtube
@@ -49,4 +49,44 @@ function searchAPIS (search, lat, lon) {
     });
 }
 
+// googlemaps update
+function updateMap (meetupResponse) {
+
+  var locations = [];
+
+      for (i = 0; i < 50; i++) {
+      	if ('venue' in meetupResponse[i]) {
+          locations.push([meetupResponse[i].name, meetupResponse[i].venue.lat, meetupResponse[i].venue.lon, meetupResponse[i].venue.name]);
+      	}
+    }
+    console.log(locations);
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 9,
+      center: new google.maps.LatLng(locations[2][1], locations[2][2]),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0] + "<br />" + locations[i][3]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+}
+
+// to have data on page load
 searchAPIS("javascript", 34.020204, -118.490765);
