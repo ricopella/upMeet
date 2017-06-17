b1e46ce75747610a41837e6b37340b68c8b81dd7
t1me1srunn1nout <hello@hovsep.io>
2017-06-17 15:48:35 -0700

    form data to append to page

--- js/updatepage.js
+++ js/updatepage.js
@@ -1,41 +1,41 @@
 console.log("updatepage.js loaded");
 
-function updatePage(meetupResponse, wikiResponse, youtubeResponse) {
+var allData = [];
 
-  meetupResponse.forEach(function(meetupItem) {
-    var meetupName = meetupItem.name;
-    var meetupDescription = meetupItem.description;
-    var startTime = meetupItem.time;
-    var meetingLength = meetupItem.duration;
-    var lat = meetupItem.group.lat;
-    var lon = meetupItem.group.lon;
-    var meetupURL = meetupItem.link;
-    var waitlist = meetupItem.waitlist_count;
+function updatePage(meetupResponse) {
 
-    console.log(meetupName, meetupDescription, startTime, meetingLength, meetupURL, waitlist);
+  allData = [];
 
-    // pull these in
-    if (meetupItem.duration !== undefined){
-      var a = meetupItem.time;
-      var b = meetupItem.duration;
-      console.log(moment(a).format("h:mm") + " - " + moment(a+b).format("hh:mm a"));
-    }
-
-    if (waitlist > 0) {
-      console.log("this one is full");
-    }
-  });
-
-  youtubeData.forEach(function(youtubeItem) {
-    console.log(youtubeItem.snippet.title);
-    console.log("https://www.youtube.com/watch?v=" + youtubeItem.id.videoId);
-    console.log(youtubeItem.snippet.thumbnails.high.url);
-  });
-
-  for (var val in wikiData) {
-      var title = wikiData[val].title;
-      var extract = wikiData[val].extract;
-      var articleURL = 'https://en.wikipedia.org/?curid='+wikiData[val].pageid;
-      console.log(title, extract, articleURL);
+  for (var i = 0; i < meetupResponse.length; i++) {
+      allData.push(allData[meetupResponse[i]] = {
+        "meetupName": meetupResponse[i].name,
+        "meetupDescription": meetupResponse[i].description,
+        "startTime": meetupResponse[i].time,
+        "meetingLength": meetupResponse[i].duration,
+        "lat": meetupResponse[i].group.lat,
+        "lon": meetupResponse[i].group.lon,
+        "meetupURL": meetupResponse[i].link,
+        "waitlist": meetupResponse[i].waitlist_count
+      });
   }
+
+  console.log(allData);
+
+  addYoutubeLinks(youtubeResponse);
 }
+
+function addYoutubeLinks(youtubeResponse){
+  for (var i = 0; i < youtubeResponse.length; i++) {
+      allData[i].youtubeTitle = youtubeResponse[i].snippet.title;
+      allData[i].youtubeURL = "https://www.youtube.com/watch?v=" + youtubeResponse[i].id.videoId;
+      allData[i].youtubeThumbnail = youtubeResponse[i].snippet.thumbnails.high.url;
+    }
+}
+
+// for (var val in wikiData) {
+//     var title = wikiData[val].title;
+//     var extract = wikiData[val].extract;
+//     var articleURL = 'https://en.wikipedia.org/?curid='+wikiData[val].pageid;
+//     console.log(title, extract, articleURL);
+//     allData
+// }
