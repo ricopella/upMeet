cca680116b8dfa35e7e3f4c7a841a067c254e1cb
Narin Sundarabhaya <nrs710@gmail.com>
2017-06-17 20:44:51 -0700

    repaired searchAPIS bug

--- js/apis.js
+++ js/apis.js
@@ -6,64 +6,65 @@ var youtubeResponse;
 var wikiResponse;
 var mapCoordinates;
 
-function searchAPIS (search, lat, lon) {
-  var query = search;
+function searchAPIS(search, lat, lon) {
+    var query = search;
 
-  // call meetup
-  $.ajax({
-    url: "https://api.meetup.com/find/events?&sign=true&photo-host=public&lon=" +
-      lon + "&text="+ query + "&radius=" + "30" + "&lat=" + lat + "&key=13493128171b80333fc956a274b1c",
-    method: "GET",
-  }).done(function(response){
-    meetupResponse = response;
-    console.log(response);
-    console.log(response.length);
-  });
+    // call meetup
+    $.ajax({
+        url: "https://api.meetup.com/find/events?&sign=true&photo-host=public&lon=" +
+            lon + "&text=" + query + "&radius=" + "30" + "&lat=" + lat + "&key=13493128171b80333fc956a274b1c",
+        method: "GET",
+    }).done(function(response) {
+        meetupResponse = response;
+        console.log(response);
+        console.log(response.length);
+    });
 
-  // call youtube
-  $.get( "https://www.googleapis.com/youtube/v3/search", {
-        part: 'snippet, id',
-        q: query,
-        type:'video',
-        'maxResults': '50',
-        key: 'AIzaSyBCZgipwmv-daOhKVQWBKISU5dGjx24rng'},
+    // call youtube
+    $.get("https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: query,
+            type: 'video',
+            'maxResults': '50',
+            key: 'AIzaSyBCZgipwmv-daOhKVQWBKISU5dGjx24rng'
+        },
 
-        function (data) {
-          console.log(data.items);
-          youtubeData = data.items;
+        function(data) {
+            console.log(data.items);
+            youtubeResponse = data.items;
         }
     );
 
     // call wikipedia
     $.ajax({
         type: 'GET',
-        dataType:'jsonp',
-        url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='+ query,
+        dataType: 'jsonp',
+        url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query,
         crossDomain: true,
         cache: false,
         success: function(json) {
             console.log(json.query.pages);
-            wikiData = json.query.pages
+            wikiResponse = json.query.pages
         }
     });
 }
 
 // googlemaps update
-function updateMap (meetupResponse) {
+function updateMap(meetupResponse) {
 
-  var locations = [];
+    var locations = [];
 
-      for (i = 0; i < 50; i++) {
-      	if ('venue' in meetupResponse[i]) {
-          locations.push([meetupResponse[i].name, meetupResponse[i].venue.lat, meetupResponse[i].venue.lon, meetupResponse[i].venue.name, meetupResponse[i].link]);
-      	}
+    for (i = 0; i < 50; i++) {
+        if ('venue' in meetupResponse[i]) {
+            locations.push([meetupResponse[i].name, meetupResponse[i].venue.lat, meetupResponse[i].venue.lon, meetupResponse[i].venue.name, meetupResponse[i].link]);
+        }
     }
     console.log(locations);
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 9,
-      center: new google.maps.LatLng(locations[2][1], locations[2][2]),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 9,
+        center: new google.maps.LatLng(locations[2][1], locations[2][2]),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
     var infowindow = new google.maps.InfoWindow();
@@ -71,20 +72,20 @@ function updateMap (meetupResponse) {
     var marker, i;
 
     for (i = 0; i < locations.length; i++) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent("<a href='" + locations[i][4] + "' target='blank'>'" + locations[i][0] + "</a>" + "<br />" + locations[i][3]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent("<a href='" + locations[i][4] + "' target='blank'>'" + locations[i][0] + "</a>" + "<br />" + locations[i][3]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
     }
     var trafficLayer = new google.maps.TrafficLayer();
-        trafficLayer.setMap(map);
+    trafficLayer.setMap(map);
 }
 
 // to have data on page load
--- js/updatepage.js
+++ js/updatepage.js
@@ -4,31 +4,31 @@ var allData = [];
 
 function updatePage(meetupResponse) {
 
-  allData = [];
+    allData = [];
 
-  for (var i = 0; i < meetupResponse.length; i++) {
-      allData.push(allData[meetupResponse[i]] = {
-        "meetupName": meetupResponse[i].name,
-        "meetupDescription": meetupResponse[i].description,
-        "startTime": meetupResponse[i].time,
-        "meetingLength": meetupResponse[i].duration,
-        "lat": meetupResponse[i].group.lat,
-        "lon": meetupResponse[i].group.lon,
-        "meetupURL": meetupResponse[i].link,
-        "waitlist": meetupResponse[i].waitlist_count
-      });
-  }
+    for (var i = 0; i < meetupResponse.length; i++) {
+        allData.push(allData[meetupResponse[i]] = {
+            "meetupName": meetupResponse[i].name,
+            "meetupDescription": meetupResponse[i].description,
+            "startTime": meetupResponse[i].time,
+            "meetingLength": meetupResponse[i].duration,
+            "lat": meetupResponse[i].group.lat,
+            "lon": meetupResponse[i].group.lon,
+            "meetupURL": meetupResponse[i].link,
+            "waitlist": meetupResponse[i].waitlist_count
+        });
+    }
 
-  console.log(allData);
+    addYoutubeLinks(youtubeResponse);
 
-  addYoutubeLinks(youtubeResponse);
+    console.log(allData);
 }
 
-function addYoutubeLinks(youtubeResponse){
-  for (var i = 0; i < youtubeResponse.length; i++) {
-      allData[i].youtubeTitle = youtubeResponse[i].snippet.title;
-      allData[i].youtubeURL = "https://www.youtube.com/watch?v=" + youtubeResponse[i].id.videoId;
-      allData[i].youtubeThumbnail = youtubeResponse[i].snippet.thumbnails.high.url;
+function addYoutubeLinks(youtubeResponse) {
+    for (var i = 0; i < youtubeResponse.length; i++) {
+        allData[i].youtubeTitle = youtubeResponse[i].snippet.title;
+        allData[i].youtubeURL = "https://www.youtube.com/watch?v=" + youtubeResponse[i].id.videoId;
+        allData[i].youtubeThumbnail = youtubeResponse[i].snippet.thumbnails.high.url;
     }
 }
 
